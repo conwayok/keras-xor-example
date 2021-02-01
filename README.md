b3a74a7e7b9421026208108c316836088e13341e
Conway <kable2580@gmail.com>
2021-02-01 18:54:26 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # keras-xor-example
-Keras training a neural network to become a xor gate
+This is a simple python script demonstrating basic deep learning with keras.
+
+## Description
+A XOR (exclusive-or) gate has inputs and outputs as follows:
+
+|A|B|A XOR B|
+|:---:|:---:|:---:|
+| 0 | 0 | 0 |
+| 0 | 1 | 1 |
+| 1 | 0 | 1 |
+| 1 | 1 | 0 |
+
+We use this as training data and try to train a XOR gate model.
--- keras-xor-example/train.py
+++ keras-xor-example/train.py
@@ -1,21 +1,22 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 import numpy as np
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Dense
 
 # create model
 model = Sequential()
 model.add(Dense(8, input_dim=2, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
-# Compile model
+# compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-# xor
+# xor dataset
 input_dataset = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 output_dataset = np.array([[0], [1], [1], [0]])
 
-# Fit the model
+# train model
 model.fit(input_dataset, output_dataset, epochs=2000, batch_size=4)
 
+# test results
 print(model.predict(input_dataset))
